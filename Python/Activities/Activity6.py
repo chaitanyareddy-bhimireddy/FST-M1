0935acb7d513e067c72073892a59cd956b9f65c9
chaitanyareddy-bhimireddy <cbhimir1@in.ibm.com>
2026-02-05 16:40:28 +0530

    Add files via upload

---
+++ Python/Activities/Activity6.py
@@ -0,0 +1,6 @@
+# Pattern Generator using nested loops
+
+for i in range(1, 10):        # numbers 1 to 9
+    for j in range(i):        # repeat i, i times
+        print(i, end="")
+    print()                   # move to next line
